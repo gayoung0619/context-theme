62dc48ecafc5336f6b03d27249b1a9c8bd56957e
gayoung <did-619@naver.com>
2025-02-12 23:30:16 +0900

    feat: add theme style

---
+++ src/theme/theme.ts
@@ -0,0 +1,14 @@
+export const lightTheme = {
+  bgcolor: '#F8F7F4',
+  textColor: '#31302E',
+  borderColor: '1px solid #eaeaea'
+}
+export const darkTheme = {
+  bgcolor: '#1E1E22',
+  textColor: '#ccc',
+  borderColor: '1px solid #2c2d33'
+}
+export const theme = {
+  lightTheme,
+  darkTheme
+}
